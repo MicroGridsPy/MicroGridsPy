2fff77d602940497c40956e3dcc2933c406847ca
FLomb <francesco.lombardi@polimi.it>
2018-10-17 14:25:42 +0200

    Deleted non-LP stuff
    Deleted files and code lines related to non-LP formulation

--- MicroGrids/Micro-Grids.py
+++ MicroGrids/Micro-Grids.py
@@ -3,18 +3,14 @@
 import pandas as pd
 from pyomo.environ import  AbstractModel
 
-from Results import Plot_Energy_Total, Load_results1,  Load_results1_binary, \
-Load_results2_binary, Energy_Mix, Print_Results, \
-Load_results1_Dispatch, Load_results2_Dispatch, Integer_Scenarios, Integer_Scenario_Information, \
-Integer_Time_Series, integer_Renewable_Energy, Integer_Data_Renewable, Integer_Generator_time_series, \
-Integer_Generator_Data, Integer_Results, Economic_Analysis
-from Model_Creation import Model_Creation, Model_Creation_binary, Model_Creation_Integer, Model_Creation_Dispatch
-from Model_Resolution import Model_Resolution, Model_Resolution_binary, Model_Resolution_Integer, Model_Resolution_Dispatch
+from Results import Plot_Energy_Total, Load_results1, Energy_Mix, Print_Results, Economic_Analysis
+from Model_Creation import Model_Creation
+from Model_Resolution import Model_Resolution
 from Economical_Analysis import Levelized_Cost_Of_Energy
 
 
 # Type of problem formulation:
-formulation = 'Integer'
+formulation = 'LP'
 
 # Renewable energy penetrarion
 
@@ -41,34 +37,8 @@ if formulation == 'LP':
     LCOE = Data[6]
     
 
-elif formulation == 'Binary':
-    Model_Creation_binary(model) # Creation of the Sets, parameters and variables.
-    instance = Model_Resolution_binary(model) # Resolution of the instance    
-    Time_Series = Load_results1_binary(instance) # Extract the results of energy from the instance and save it in a excel file 
-    Results = Load_results2_binary(instance) # Save results into a excel file
-elif formulation =='Integer':
-    Model_Creation_Integer(model, Renewable_Penetration, Battery_Independency)
-    instance = Model_Resolution_Integer(model,Renewable_Penetration, Battery_Independency)
-    Scenarios = Integer_Scenarios(instance) # Extract the results of energy from the instance and save it in a excel file 
-    Scenario_Information = Integer_Scenario_Information(instance)
-    
-    Renewable_Energy = integer_Renewable_Energy(instance, Scenarios)
-    Data_Renewable = Integer_Data_Renewable(instance)
-    Generator_Time_Series = Integer_Generator_time_series(instance, Scenarios)
-    Generator_Data = Integer_Generator_Data(instance)
-    Results = Integer_Results(instance)
-    
-    NPC,LCOE = Economic_Analysis(Scenarios, Scenario_Information, Renewable_Energy, Data_Renewable,
-                      Generator_Time_Series, Generator_Data, Results)
-    print(NPC)
-    print(LCOE)
-    Scenario_Probability = Scenario_Information.loc['Scenario Weight']  
-    
-elif formulation =='Dispatch':
-    Model_Creation_Dispatch(model)
-    instance = Model_Resolution_Dispatch(model)
-    Time_Series = Load_results1_Dispatch(instance) # Extract the results of energy from the instance and save it in a excel file 
-    Results = Load_results2_Dispatch(instance)
+else:
+    print('Model formulation type not allowed')
 
      
 # Energy Plot    
