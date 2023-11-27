b0b3792405b1e2e2242740923662241074e81e36
GiacomoCrevani <giacomo.crevani@mail.polimi.it>
2023-11-27 13:57:41 +0100

    Small changes to plot function
    Simplified plots.py
    Edited main default plots

--- Code/Micro-Grids.py
+++ Code/Micro-Grids.py
@@ -48,28 +48,28 @@ PlotTime = 3                         # Number of days to be shown in the plot
 PlotFormat = 'png'                   # Desired extension of the saved file (Valid formats: png, svg, pdf)
 PlotResolution = 400                 # Plot resolution in dpi (useful only for .png files, .svg and .pdf output a vector plot)
 
-PlotScenario1 = 1                    # Plot scenario
-PlotDate1 = '01/01/2026 00:00:00'    # Month-Day-Year. If devoid of meaning: Day-Month-Year
-PlotTime1 = 3                        # Number of days to be shown in the plot
-PlotFormat1 = 'png'                  # Desired extension of the saved file (Valid formats: png, svg, pdf)
-PlotResolution1 = 400                # Plot resolution in dpi (useful only for .png files, .svg and .pdf output a vector plot)
+# PlotScenario1 = 1                    # Plot scenario
+# PlotDate1 = '01/01/2026 00:00:00'    # Month-Day-Year. If devoid of meaning: Day-Month-Year
+# PlotTime1 = 3                        # Number of days to be shown in the plot
+# PlotFormat1 = 'png'                  # Desired extension of the saved file (Valid formats: png, svg, pdf)
+# PlotResolution1 = 400                # Plot resolution in dpi (useful only for .png files, .svg and .pdf output a vector plot)
 
-PlotScenario2 = 1                    # Plot scenario
-PlotDate2 = '01/01/2032 00:00:00'    # Month-Day-Year. If devoid of meaning: Day-Month-Year
-PlotTime2 = 3                        # Number of days to be shown in the plot
-PlotFormat2 = 'png'                  # Desired extension of the saved file (Valid formats: png, svg, pdf)
-PlotResolution2 = 400                # Plot resolution in dpi (useful only for .png files, .svg and .pdf output a vector plot)
+# PlotScenario2 = 1                    # Plot scenario
+# PlotDate2 = '01/01/2032 00:00:00'    # Month-Day-Year. If devoid of meaning: Day-Month-Year
+# PlotTime2 = 3                        # Number of days to be shown in the plot
+# PlotFormat2 = 'png'                  # Desired extension of the saved file (Valid formats: png, svg, pdf)
+# PlotResolution2 = 400                # Plot resolution in dpi (useful only for .png files, .svg and .pdf output a vector plot)
 
-PlotScenario3 = 1                    # Plot scenario
-PlotDate3 = '01/01/2037 00:00:00'    # Month-Day-Year. If devoid of meaning: Day-Month-Year
-PlotTime3 = 3                        # Number of days to be shown in the plot
-PlotFormat3 = 'png'                  # Desired extension of the saved file (Valid formats: png, svg, pdf)
-PlotResolution3 = 400                # Plot resolution in dpi (useful only for .png files, .svg and .pdf output a vector plot)
+# PlotScenario3 = 1                    # Plot scenario
+# PlotDate3 = '01/01/2037 00:00:00'    # Month-Day-Year. If devoid of meaning: Day-Month-Year
+# PlotTime3 = 3                        # Number of days to be shown in the plot
+# PlotFormat3 = 'png'                  # Desired extension of the saved file (Valid formats: png, svg, pdf)
+# PlotResolution3 = 400                # Plot resolution in dpi (useful only for .png files, .svg and .pdf output a vector plot)
 
 DispatchPlot(instance,TimeSeries,PlotScenario,PlotDate,PlotTime,PlotResolution,PlotFormat)
-#DispatchPlot1(instance,TimeSeries,PlotScenario1,PlotDate1,PlotTime1,PlotResolution1,PlotFormat1)
-#DispatchPlot2(instance,TimeSeries,PlotScenario2,PlotDate2,PlotTime2,PlotResolution2,PlotFormat2)
-#DispatchPlot3(instance,TimeSeries,PlotScenario3,PlotDate3,PlotTime3,PlotResolution3,PlotFormat3)
+#DispatchPlot(instance,TimeSeries,PlotScenario1,PlotDate1,PlotTime1,PlotResolution1,PlotFormat1)
+#DispatchPlot(instance,TimeSeries,PlotScenario2,PlotDate2,PlotTime2,PlotResolution2,PlotFormat2)
+#DispatchPlot(instance,TimeSeries,PlotScenario3,PlotDate3,PlotTime3,PlotResolution3,PlotFormat3)
 #CashFlowPlot(instance,Results,PlotResolution,PlotFormat)
 #SizePlot(instance,Results,PlotResolution,PlotFormat)
 
