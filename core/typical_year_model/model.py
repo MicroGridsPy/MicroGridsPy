dc20f0f97dfb54eebfbaf3fe033ac2e44a4d8709
AleOnori98 <onorialessandro1998@gmail.com>
2026-04-13 22:40:49 +0200

    Update model.py

--- core/typical_year_model/model.py
+++ core/typical_year_model/model.py
@@ -153,6 +153,12 @@ class SteadyStateModel:
             encoding="utf-8",
         )
 
+    def _solve_supports_kwarg(self, name: str) -> bool:
+        solve_fn = getattr(self.model, "solve", None)
+        code = getattr(solve_fn, "__code__", None)
+        varnames = getattr(code, "co_varnames", ())
+        return bool(varnames) and name in varnames
+
     # ---------------------------------------------------------------------
     # Solve
     # ---------------------------------------------------------------------
@@ -188,9 +194,9 @@ class SteadyStateModel:
         try:
             # Many linopy versions accept log_file / logfile; be defensive
             if self._last_log_path is not None:
-                if "logfile" in self.model.solve.__code__.co_varnames:
+                if self._solve_supports_kwarg("logfile"):
                     solve_kwargs["logfile"] = str(self._last_log_path)
-                elif "log_file" in self.model.solve.__code__.co_varnames:
+                elif self._solve_supports_kwarg("log_file"):
                     solve_kwargs["log_file"] = str(self._last_log_path)
                 # otherwise: ignore; solver may still print to stdout
         except Exception:
@@ -198,11 +204,13 @@ class SteadyStateModel:
             pass
 
         # Solve call
+        solve_fn = self.model.solve
+        use_solver_name = self._solve_supports_kwarg("solver_name")
         if self._last_log_path is not None:
             with tee_console_output(self._last_log_path):
-                result = self.model.solve(solver_name=solver, **solve_kwargs) if "solver_name" in getattr(self.model.solve, "__code__", object()).co_varnames else self.model.solve(solver, **solve_kwargs)
+                result = solve_fn(solver_name=solver, **solve_kwargs) if use_solver_name else solve_fn(solver, **solve_kwargs)
         else:
-            result = self.model.solve(solver_name=solver, **solve_kwargs) if "solver_name" in getattr(self.model.solve, "__code__", object()).co_varnames else self.model.solve(solver, **solve_kwargs)
+            result = solve_fn(solver_name=solver, **solve_kwargs) if use_solver_name else solve_fn(solver, **solve_kwargs)
         self._ensure_log_artifact(solver)
 
         # Linopy stores solution on the model
